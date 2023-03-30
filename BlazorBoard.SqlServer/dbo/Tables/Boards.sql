627779b8eb5f6197f41ae30ce0331ccdb5d2365a
Seolhee <seolhee@campigotto.com>
2023-03-30 00:58:39 -0400

    Candidate List page

---
+++ BlazorBoard.SqlServer/dbo/Tables/Boards.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Boards] (
+    [Id]      INT           IDENTITY (1, 1) NOT NULL,
+    [Name]    NVARCHAR (25) NOT NULL,
+    [BrothId] INT           NOT NULL,
+    CONSTRAINT [PK_Boards] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Boards_Broths_BrothId] FOREIGN KEY ([BrothId]) REFERENCES [dbo].[Broths] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Boards_BrothId]
+    ON [dbo].[Boards]([BrothId] ASC);
+
---
+++ BlazorBoard.SqlServer/dbo/Tables/Garnishes.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Garnishes] (
+    [Id]      INT           IDENTITY (1, 1) NOT NULL,
+    [Name]    NVARCHAR (25) NOT NULL,
+    [BoardId] INT           NULL,
+    CONSTRAINT [PK_Garnishes] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Garnishes_Boards_BoardId] FOREIGN KEY ([BoardId]) REFERENCES [dbo].[Boards] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Garnishes_BoardId]
+    ON [dbo].[Garnishes]([BoardId] ASC);
+
