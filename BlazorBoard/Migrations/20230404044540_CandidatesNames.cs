336dee7ea652445116f467125530f4b747d4c178
Seolhee <79366075+SeolheeKim-developer@users.noreply.github.com>
2023-04-04 00:53:52 -0400

    ERROR

---
+++ BlazorBoard/Migrations/20230404044540_CandidatesNames.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorBoard.Migrations
+{
+    /// <inheritdoc />
+    public partial class CandidatesNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_candidatesNames",
+                table: "candidatesNames");
+
+            migrationBuilder.RenameTable(
+                name: "candidatesNames",
+                newName: "CandidatesNames");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CandidatesNames",
+                table: "CandidatesNames",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CandidatesNames",
+                table: "CandidatesNames");
+
+            migrationBuilder.RenameTable(
+                name: "CandidatesNames",
+                newName: "candidatesNames");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_candidatesNames",
+                table: "candidatesNames",
+                column: "Id");
+        }
+    }
+}
